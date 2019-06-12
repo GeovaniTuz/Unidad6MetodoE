de8c17a1c0f4792e5f46720e0189a1006faa28e1
GeovaniTuz <geovani.tuzpoot@itsva.edu.mx>
2019-06-12 18:18:03 -0500

    Metodo de E

---
+++ Unidad6b/src/unidad6b/Metodos_Numericos_TI.java
@@ -0,0 +1,15 @@
+package unidad6b;
+
+/**
+ *
+ * @author 
+ */
+public class Metodos_Numericos_TI {
+
+    public static void main(String[] args) {
+    Inicio Ini=new Inicio();
+    Ini.setVisible(true);
+    Ini.setLocationRelativeTo(null);
+    }
+    
+}
